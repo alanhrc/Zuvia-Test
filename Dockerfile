bf29dc099dbd9d6fbf785c47e0f622e6f06b6837
Alan Henrique <alancamargo50@gmail.com>
2023-04-24 00:34:31 -0300

    Adjusts docker compose e dockerfile to configure database mongodb

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,11 @@ ARG DK_UID=1000
 ARG DK_GID=1000
 
 RUN apk --no-cache add shadow && \
-    usermod -u ${DK_UID:-1000} node && \
-    groupmod -g ${DK_GID:-1000} node
+  usermod -u ${DK_UID:-1000} node && \
+  groupmod -g ${DK_GID:-1000} node
 
 USER node
 
 RUN mkdir /home/node/project
 
 WORKDIR /home/node/project
-
-EXPOSE 3333
-
-CMD ["npm", "run", "start:prod"]
--- remove_volumes.sh
+++ remove_volumes.sh
@@ -1,7 +1,6 @@
 docker compose down
-find ./docker/volumes/mssql ! -name '.gitignore' -delete
-find ./docker/volumes/nosqldb ! -name '.gitignore' -delete
+find ./docker/volumes/nosqldb ! -name '.gitkeep' -delete
 docker rm $(docker ps -a -q)
-# docker rmi $(docker images -a -q)
+docker rmi $(docker images -a -q)
 docker volume prune --force
-# docker builder prune --force
+docker builder prune --force
