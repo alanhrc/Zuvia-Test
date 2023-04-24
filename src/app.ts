c2d5b0e6ec808c786cf5c3ce129b4602328e575f
Alan Henrique <alancamargo50@gmail.com>
2023-04-24 09:17:22 -0300

    Add docs to application

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import 'dotenv/config'
 
 import express, { Request, Response, NextFunction } from 'express';
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger/swagger.json'
 
 import './shared/container';
 import { router } from './routes';
@@ -11,7 +13,7 @@ import { AppError } from './shared/errors/AppError';
 const app = express();
 
 app.use(express.json());
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
